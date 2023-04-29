0ee82fef0626b51229c26071bee97151b71eb46c
vaibhavkrishanyadav <vaibhavkrishanyadav@gmail.com>
2023-04-29 23:25:58 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+
+// express app
+const app = express();
+
+// listen for requests
+app.listen(3000);
+
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
+app.get('/', (req, res) => {
+  const blogs = [
+    {title: 'Lorem ipsum', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Lorem ipsum', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Lorem ipsum', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  console.log('index');
+  res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+  console.log('about');
+  res.render('about', { title: 'About' });
+});
+
+app.get('/blogs/create', (req, res) => {
+  console.log('blog');
+  res.render('create', { title: 'Create a new blog' });
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
