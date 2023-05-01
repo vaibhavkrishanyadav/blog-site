877b288e5db3a51db594d2a20da3dfa48d79a19d
vaibhavkrishanyadav <vaibhavkrishanyadav@gmail.com>
2023-05-01 18:58:36 +0530

    added blogs model and connected to db

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+const passKeys = require('./pass-keys');
 
 // express app
 const app = express();
 
-// listen for requests
-app.listen(3000);
+mongoose.connect(passKeys.dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
 
 // register view engine
 app.set('view engine', 'ejs');
@@ -15,20 +20,36 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+// app.get('/add-blog', (req, res) => {
+//   const blog = Blog({
+//     title: 'my blog 2',
+//     snippet: 'Lorem ipsum dolor sit amet consectetur',
+//     body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quia quibusdam quaerat illo a modi tenetur ut blanditiis, illum quo consectetur recusandae excepturi natus impedit rem, tempora cum fuga quae in.',
+//   });
+
+//   blog.save()
+//     .then((result) => {
+//       res.send(result);
+//     })
+//     .catch((err) => console.log(err))
+// })
+
 app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Lorem ipsum', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Lorem ipsum', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Lorem ipsum', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  console.log('index');
-  res.render('index', { title: 'Home', blogs });
+  res.redirect('/blogs');
 });
 
 app.get('/about', (req, res) => {
   console.log('about');
   res.render('about', { title: 'About' });
 });
+
+app.get('/blogs', (req, res) => {
+  Blog.find().sort({ createdAt: -1})
+    .then((result) => {
+      res.render('index', { title: 'All-blogs', blogs: result})
+    })
+    .catch((err) => console.log(err))
+})
 
 app.get('/blogs/create', (req, res) => {
   console.log('blog');
